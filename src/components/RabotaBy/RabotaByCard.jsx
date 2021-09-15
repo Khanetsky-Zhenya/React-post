376f72c0340047e48fe01a09aebbd810acd21a02
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-09-15 08:41:02 +0300

    fix:fix style with MODULE for task #3

--- src/components/RabotaBy/RabotaByCard.jsx
+++ src/components/RabotaBy/RabotaByCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import style from './style.css'
+import styles from './RabotaByCard.module.css'
 export class RabotaByCard extends React.Component {
     render (){
         const { title, nameOrg, region, mainDescription, secondDescription, date } = this.props;
-        return <div className="wrapper">
-            <div className="vacancy" >
-                <h2 className="vacancy-title">{`${title}`}</h2>
-                <p className="vacancy-nameOrg">{`${nameOrg}`} &#10003;</p>
-                <p className="vacancy-region">{`${region}`}</p>
-                <p className="vacancy-mainDescription">{`${mainDescription}`}</p>
-                <p className="vacancy-secondDescription">{`${secondDescription}`}</p>
-                <div className="vacancy-footer">
-                    <button>Откликнуться</button>
-                    <p className="vacancy-date">{`${date}`}</p>
+        return <div className={styles.wrapper}>
+            <div className={styles.vacancy}>
+                <h2 className={styles.vacancy_title}>{`${title}`}</h2>
+                <p className={styles.vacancy_nameOrg}>{`${nameOrg}`} &#10003;</p>
+                <p className={styles.vacancy_region}>{`${region}`}</p>
+                <p className={styles.vacancy_mainDescription}>{`${mainDescription}`}</p>
+                <p className={styles.vacancy_secondDescription}>{`${secondDescription}`}</p>
+                <div className={styles.vacancy_footer}>
+                    <button className={styles.vacancy_button}>Откликнуться</button>
+                    <p className={styles.vacancy_date}>{`${date}`}</p>
                 </div>
             </div>
         </div>
