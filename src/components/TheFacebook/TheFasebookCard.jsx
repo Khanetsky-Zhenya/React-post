684cfbf800001ef872c6c6b9f8152b5e05e1f50d
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-09-15 08:15:57 +0300

    fix:fix style with MODULE for task #1

--- src/components/TheFacebook/TheFasebookCard.jsx
+++ src/components/TheFacebook/TheFasebookCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import style from './style.css'
+import styles from './TheFasebookCard.module.css'
 export class TheFacebookCard extends React.Component {
     render (){
         const { avatarUrl, firstName, secondName, career, text, like, reply, time } = this.props;
-        return <div className="wrapper">
-            <div className="post">
-                <img className="post-img"src={avatarUrl} alt="photo user" />
-                <div className="info">
-                    <div className="info-title">
-                        <h2 className="info-title__name">{`${firstName}`}</h2>
-                        <h2 className="info-title__secondName">{`${secondName}`} </h2>
-                        <h2 className="info-title__career">&middot; {`${career}`}</h2>
+        return <div className={styles.wrapper}>
+            <div className={styles.post}>
+                <img className={styles.post_img} src={avatarUrl} alt="photo user" />
+                <div className={styles.info}>
+                    <div className={styles.info_title}>
+                        <h2 className={styles.info_title__name}>{`${firstName}`}</h2>
+                        <h2 className={styles.info_title__secondName}>{`${secondName}`} </h2>
+                        <h2 className={styles.info_title__career}>&middot; {`${career}`}</h2>
                     </div>
-                    <p  className="info-text">{`${text}`}</p>
-                    <div className="info-subtitle">
-                    <p  className="info-subtitle__like">{`${like}`} &middot;</p>
-                    <p  className="info-subtitle__reply">{`${reply}`} &middot;</p>
-                    <p  className="info-subtitle__time">{`${time}`}</p>
+                    <p  className={styles.info_text}>{`${text}`}</p>
+                    <div className={styles.info_subtitle}>
+                    <p  className={styles.info_subtitle__like}>{`${like}`} &middot;</p>
+                    <p  className={styles.info_subtitle__reply}>{`${reply}`} &middot;</p>
+                    <p  className={styles.info_subtitle__time}>{`${time}`}</p>
                     </div>
                 </div>
             </div>
